0861dc0d8bbeacea3ca95ec6d93bd64af2eb78f9
baiemir <baemir.omurzak0v@mail.ru>
2025-11-28 19:52:17 +0600

    Create all doesnt need more

--- app/main.py
+++ app/main.py
@@ -7,8 +7,6 @@ from .api.v1.product import router as product_router_v1
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    async with db_helper.engine.begin() as conn:
-        await conn.run_sync(Base.metadata.create_all)
     yield
 
 
