572f0c462c43fe2f58747632598cfe503f8ce22d
baiemir <baemir.omurzak0v@mail.ru>
2025-12-02 14:29:22 +0600

    Was added battery schema for phone

---
+++ app/phone/schemas_v1/battery.py
@@ -0,0 +1,31 @@
+from pydantic import BaseModel, ConfigDict
+
+
+class BatteryBase(BaseModel):
+    capacity: int
+
+    fast_charging: bool
+
+    wireless_charging: bool
+
+    charging_connector: str
+
+
+class BatteryCreate(BatteryBase):
+    pass
+
+
+class BatteryUpdate(BaseModel):
+    capacity: int | None = None
+
+    fast_charging: bool | None = None
+
+    wireless_charging: bool | None = None
+
+    charging_connector: str | None = None
+
+
+class BatteryOut(BatteryBase):
+    id: int
+
+    model_config = ConfigDict(from_attributes=True)
