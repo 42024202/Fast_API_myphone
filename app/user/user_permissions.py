1a7ae35516b8ea8725f599594d08813f1c622194
baiemir <baemir.omurzak0v@mail.ru>
2025-12-04 16:47:00 +0600

    Fix bag with router

--- app/user/user_permissions.py
+++ app/user/user_permissions.py
@@ -3,7 +3,7 @@ from app.user.models.user import UserRole
 from app.user.services import fastapi_users
 
 
-def admin_only(user=Depends(fastapi_users.current_user)):
+def admin_only(user=Depends(fastapi_users.current_user())):
     if user.role != UserRole.ADMIN:
         raise HTTPException(
                 status_code=403,
